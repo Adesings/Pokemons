2e58760f2075e37f96f1daf7734ddeedadb51c9e
Alejandro <nichox.ojeda@gmail.com>
2017-12-20 15:48:41 -0300

    50% de avance del proyecto

---
+++ Pokémons/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pokémons.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Formulario()
+        {
+            ViewBag.Message = "Ingreso de Pokémons.";
+
+            return View();
+        }
+
+        public ActionResult Listado()
+        {
+            ViewBag.Message = "Listado de Pokémons";
+
+            return View();
+        }
+
+        public ActionResult Ficha()
+        {
+            ViewBag.Message = "Ficha del Entrenador Pokémon.";
+
+            return View();
+        }
+    }
+}
---
+++ Pokémons/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pokémons.MvcApplication" Language="C#" %>
